c4c84bb6baa65fa798385624bf9b4338c105f68e
Samin Chowdhury <samin@Samins-MacBook-Air.local>
2021-05-04 14:30:22 +0200

    Updated the Game class and added canvas to the Display class

--- src/Display.java
+++ src/Display.java
@@ -1,4 +1,5 @@
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
@@ -6,18 +7,22 @@ import javax.swing.JFrame;
 public class Display extends Canvas {
     private JFrame frame;
     private Dimension size;
+    private Canvas canvas;
+  
 
     private static String title = "No man's budget";
-    static final int WIDTH = 800;
-    static final int HEIGHT = 600;
+    static final int WIDTH = 1000;
+    static final int HEIGHT = 800;
 
     public Display() {
         // Creates the window's frame and dimension
         this.frame = new JFrame();
         this.size = new Dimension(WIDTH, HEIGHT);
+        
 
         // Sets the window's title
         this.frame.setTitle(title);
+        
 
         // Sets the window's size
         this.setPreferredSize(size);
@@ -31,7 +36,14 @@ public class Display extends Canvas {
         this.frame.setLocationRelativeTo(null);
         this.frame.setResizable(false);
 
+        this.frame.setBackground(Color.CYAN);
+
         // Enable visibility
         this.frame.setVisible(true);
+
+        canvas = new Canvas();
+        canvas.setPreferredSize(size);
+
+        frame.add(canvas);
     }
 }
--- src/NoMansBudget.java
+++ src/NoMansBudget.java
@@ -5,17 +5,71 @@
  * explore in a two dimensional setting.
  * 
  * @author Love Lindgren
- * @author Samin
+ * @author Samin Chowdhury
  * @version 2021-05-04
  */
-public class NoMansBudget {
+public class NoMansBudget implements Runnable {
     private Display display;
+    private boolean run = false;
+    private Thread thread;
+
 
     public NoMansBudget() {
-        this.display = new Display();
+        start();
     }
 
     public static void main(String[] args) {
         NoMansBudget program = new NoMansBudget();
     }
+    // updates the state of the game 
+    private void update(){
+          
+        
+    }
+    // renders the update to the display
+    private void render(){
+
+    }
+    private void initialize(){
+        this.display = new Display();
+
+    }
+
+    public void run(){
+        // Opens the game window
+        initialize();  
+        // The game loop. Runs until run becomes false
+        while(run){
+            update();
+            render();
+
+        }
+        stop();
+
+    }
+    // Calling this method starts the game in a new thread
+    public synchronized void start(){
+        // do nothing if the game is already running
+        if(run){
+            return;
+        }
+        thread = new Thread(this);
+        thread.start();
+
+    }
+    // stops the game
+    public synchronized void stop(){
+        if(!run){
+            return;
+        }
+        try{
+            thread.join();
+        }
+        catch (Exception e){
+
+        }
+        
+        
+    }
 }
+

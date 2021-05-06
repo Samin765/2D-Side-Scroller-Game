91b682660588065def1e303fc1145afcaee1718d
Samin Chowdhury <samin@Samins-MacBook-Air.local>
2021-05-06 11:12:11 +0200

    Updated so code is the same as solarsystem

--- src/Display.java
+++ src/Display.java
@@ -2,28 +2,38 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+/**
+ * Creates a window with JFrame. The title, position, size, color/image and
+ * closeability of the window is predetermined.
+ * 
+ * @author Love Lindgren
+ * @author Samin Chowdhury
+ * @version 2021-05-04
+ */
 public class Display extends Canvas {
     private JFrame frame;
     private Dimension size;
     private Canvas canvas;
-  
 
     private static String title = "No man's budget";
-    static final int WIDTH = 1000;
-    static final int HEIGHT = 800;
+    static final int WIDTH = 1280;
+    static final int HEIGHT = 720;
 
-    public Display() {
+    public Display(Color color) {
         // Creates the window's frame and dimension
         this.frame = new JFrame();
         this.size = new Dimension(WIDTH, HEIGHT);
 
-        
-
         // Sets the window's title
         this.frame.setTitle(title);
-        
 
         // Sets the window's size
         this.setPreferredSize(size);
@@ -37,14 +47,43 @@ public class Display extends Canvas {
         this.frame.setLocationRelativeTo(null);
         this.frame.setResizable(false);
 
-        this.frame.setBackground(Color.CYAN);
+        // Sets the window's background colour
+        this.frame.setBackground(color);
 
         // Enable visibility
         this.frame.setVisible(true);
+    }
 
-        canvas = new Canvas();
-        canvas.setPreferredSize(size);
+    /**
+     * Creates a window with an image as a background
+     * 
+     * @param img
+     */
+    public Display(String img) {
+        // Creates the window's frame and dimension
+        this.frame = new JFrame();
 
-        frame.add(canvas);
+        // Sets the window's title
+        this.frame.setTitle(title);
+
+        // Sets the window's background image
+        try {
+            this.frame.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File(img)))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.frame.add(this);
+        this.frame.pack();
+
+        // Enables shutdown of program
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Sets the window's location to center
+        this.frame.setLocationRelativeTo(null);
+        this.frame.setResizable(false);
+
+        // Enable visibility
+        this.frame.setVisible(true);
     }
 }

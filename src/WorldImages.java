73af1bd46532d9ba5a5dd4d07082e51ecf53cfd1
Samin Chowdhury <samin@Samins-MacBook-Air.local>
2021-05-09 17:34:57 +0200

    Added "WorldImages" class
    Use one big images instead of multiple so the game runs more efficient.

---
+++ src/WorldImages.java
@@ -0,0 +1,14 @@
+import java.awt.image.BufferedImage;
+
+public class WorldImages {
+    private BufferedImage image;
+
+
+    public WorldImages(BufferedImage image){
+        this.image = image;
+    }
+
+    public BufferedImage crop(int x, int y, int width, int height){
+        return image.getSubimage(x,y,width,height); // returns a portion of the image so you can "move around in the world"
+    }
+}

f99671f0efc069d8c81c9976a66ea4f9660da9c2
Samin Chowdhury <samin@Samins-MacBook-Air.local>
2021-05-09 19:26:34 +0200

    added image

--- src/Resources.java
+++ src/Resources.java
@@ -2,13 +2,14 @@ import java.awt.image.BufferedImage;
 
 public class Resources{
 
-    private static final int height = 500, width = 220;
+    private static final int width = 1280, height = 720; 
     public static BufferedImage solarSystemBackground, marsBackground;
 
     public static void init(){
-        WorldImages worldImage = new WorldImages(ImageLoader.loadImage("../marsBackground.png"));
+        WorldImages worldImage = new WorldImages(ImageLoader.loadImage("../spaceStars.jpeg"));  // fixa en stor bild med alla bilder vi ska ha med samma width och height
 
-         marsBackground = worldImage.crop(0,80, width, height);
+        marsBackground = ImageLoader.loadImage("../marsBackground.png");
+         solarSystemBackground = worldImage.crop(0,0, width, height); // delar upp den stora bilden till små. 
 
     }
 }

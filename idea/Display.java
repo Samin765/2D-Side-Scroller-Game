9a56f5fb4d827a6de689691a00680ae2e1df1c9c
Love Lindgren <lolindgr@kth.se>
2021-05-15 23:58:15 +0200

    Added transparent, but clickable button

--- idea/Display.java
+++ idea/Display.java
@@ -10,31 +10,42 @@ import javax.swing.JButton;
 
 public class Display extends Canvas implements ActionListener {
     private JFrame frame;
-    private JButton button;
-    private JButton button2;
     private Dimension size;
+
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
 
     private static String title = "No man's budget";
     static final int WIDTH = 1280;
     static final int HEIGHT = 720;
 
     public Display() {
-        this.button = new JButton("Boton");
-        this.button.setBounds(590, 310, 100, 100);
-        this.button.addActionListener(this);
+        this.button1 = new JButton("Boton");
+        this.button1.setBounds(590, 310, 100, 100);
+        this.button1.addActionListener(this);
 
         this.button2 = new JButton("New Boton");
         this.button2.setBounds(400, 335, 100, 50);
         this.button2.setVisible(false);
         this.button2.addActionListener(this);
 
+        this.button3 = new JButton("Last boton");
+        this.button3.setBounds(590, 200, 100, 100);
+        this.button3.setOpaque(false);
+        this.button3.setContentAreaFilled(false);
+        this.button3.setBorderPainted(false);
+        this.button3.addActionListener(this);
+
         this.frame = new JFrame(title);
 
         this.size = new Dimension(WIDTH, HEIGHT);
         this.setPreferredSize(size);
 
-        this.frame.add(this.button);
+        this.frame.add(this.button1);
         this.frame.add(this.button2);
+        this.frame.add(this.button3);
+
         this.frame.add(this);
         this.frame.pack();
 
@@ -48,11 +59,13 @@ public class Display extends Canvas implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == this.button) {
+        if (e.getSource() == this.button1) {
             this.button2.setVisible(true);
             System.out.println("Poo");
-        } else {
+        } else if (e.getSource() == this.button2) {
             System.out.println("Piss");
+        } else {
+            System.out.println("Fart");
         }
     }
 

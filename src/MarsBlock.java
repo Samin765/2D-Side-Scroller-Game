975a070c3ea3b9846c108decb62f8b6a86ebbcfd
saminc <saminc@kth.se>
2021-05-20 12:50:10 +0200

    Adding blocks that builds up the world

---
+++ src/MarsBlock.java
@@ -0,0 +1,16 @@
+
+public class MarsBlock extends WorldBlocks{
+    public MarsBlock(int id){
+        super(Resources.marsBlock, id, 200, 200);
+    }
+
+  
+    public boolean isSolid(){
+        return true;
+    }
+
+
+}
+
+
+
---
+++ src/MarsBridge.java
@@ -0,0 +1,16 @@
+
+public class MarsBridge extends WorldBlocks{
+    public MarsBridge(int id){
+        super(Resources.marsBridge, id, 400, 200);
+    }
+
+  
+    public boolean isSolid(){
+        return true;
+    }
+
+
+}
+
+
+
